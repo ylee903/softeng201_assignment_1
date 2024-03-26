d8e71c59615d46f37976153060dee74791436327
ylee903 <79842277+ylee903@users.noreply.github.com>
2024-03-26 22:07:09 +1300

    it is not compiling again, but I think on on the correct track with the printmessage ucntion

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -36,14 +36,8 @@ public class VenueHireSystem {
 
     // This prints the details of each venue in the list
     for (Venue venue : venuesActualListOfVenues) {
-      String output =
-          String.format(
-              MessageCli.VENUE_ENTRY.getMessage(),
-              venue.getVenueName(),
-              venue.getVenueCode(),
-              venue.getCapacity(),
-              venue.getHireFee());
-      MessageCli.VENUE_ENTRY.printMessage(output);
+      MessageCli.VENUE_ENTRY.printMessage(
+          venue.getVenueName(), venue.getVenueCode(), venue.getCapacity(), venue.getHireFee());
     }
 
     /*
