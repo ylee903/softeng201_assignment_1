35fd138893a44caa82cfb5bcb985026704f6ff60
ylee903 <79842277+ylee903@users.noreply.github.com>
2024-03-26 23:01:18 +1300

    formalised logic for printing in words

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -26,17 +26,6 @@ public class VenueHireSystem {
 
     if (venuesActualListOfVenues.isEmpty()) {
       MessageCli.NO_VENUES.printMessage();
-
-    } else if (venuesActualListOfVenues.size() == 1) {
-      System.out.println("There is one venue in the system:");
-    } else if (venuesActualListOfVenues.size() < 10) {
-
-      System.out.println(
-          "There are "
-              + numbersInWords[venuesActualListOfVenues.size()]
-              + " venues in the system:");
-    } else {
-      System.out.println("There are " + venuesActualListOfVenues.size() + " venues in the system:");
     }
 
     // This prints the details of each venue in the list, the date available will be added later
